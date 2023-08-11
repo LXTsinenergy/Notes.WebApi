cc727e14c8961250abb0bfa3f679b43437de6d63
LXTsinenergy <bellerstas3002@gmail.com>
2023-08-11 22:53:55 +0300

    Создание контекста базы данных

---
+++ Notes.Persistense/DatabaseContext/INotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Persistense.DatabaseContext
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Persistense/DatabaseContext/NotesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Persistense.EntityTypeConfigurations;
+
+namespace Notes.Persistense.DatabaseContext
+{
+    public sealed class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
