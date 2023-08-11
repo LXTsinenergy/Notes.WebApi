ae1644299113bc3f8471bd730bf8bbd29c0eee8e
LXTsinenergy <bellerstas3002@gmail.com>
2023-08-11 22:56:26 +0300

    Создание класса-инициализатора БД

---
+++ Notes.Persistense/DatabaseControls/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Notes.Persistense.DatabaseContext;
+
+namespace Notes.Persistense.DatabaseControls
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context) => 
+            context.Database.EnsureCreated();
+
+    }
+}
