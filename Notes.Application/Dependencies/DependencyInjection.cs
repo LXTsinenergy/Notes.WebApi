33b77b9fdf237145d2516320228d206d78cc699e
LXTsinenergy <bellerstas3002@gmail.com>
2023-08-12 15:16:01 +0300

    Внедрение MediatR в Application слой

---
+++ Notes.Application/Dependencies/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Notes.Application.Dependencies
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
