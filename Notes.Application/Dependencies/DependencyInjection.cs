4e08eeea86a92642062c23b541f5cd5bf2178d7d
LXTsinenergy <bellerstas3002@gmail.com>
2023-08-13 01:04:55 +0300

    Добавление в сервисы валидатора из текущей сборки и зависимости ValidationBehavior от IPipelineBehavior

--- Notes.Application/Dependencies/DependencyInjection.cs
+++ Notes.Application/Dependencies/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Common.Behavior;
 using System.Reflection;
 
 namespace Notes.Application.Dependencies
@@ -8,6 +11,9 @@ namespace Notes.Application.Dependencies
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
             return services;
         }
     }
