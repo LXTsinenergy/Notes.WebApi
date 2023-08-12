a710e7a94f6517fb01f16920e5199133e4db0e38
LXTsinenergy <bellerstas3002@gmail.com>
2023-08-12 15:07:41 +0300

    Создание метода расширения AddPersistense в классе DeoendencyInjection для внедрения зависимости контекста БД

---
+++ Notes.Persistense/Dependencies/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Persistense.DatabaseContext;
+
+namespace Notes.Persistense.Dependencies
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<INotesDbContext, NotesDbContext>();
+            return services;
+        }
+    }
+}
